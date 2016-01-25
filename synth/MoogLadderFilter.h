aab23edd48f791cf5652a9871988b16f297e592e
nodigga <nodigga@gmail.com>
2016-01-24 18:22:04 -0800

    Added Controls For MoogLadder

---
+++ synth/MoogLadderFilter.h
@@ -0,0 +1,13 @@
+//
+//  MoogLadderFilter.h
+//  synth
+//
+//  Created by Nicholas Cardinal on 1/24/16.
+//  Copyright © 2016 nicholas.cardinal. All rights reserved.
+//
+
+#import "AKInstrument.h"
+
+@interface MoogLadderFilter : AKInstrument
+
+@end
---
+++ synth/MoogLadderFilter.m
@@ -0,0 +1,13 @@
+//
+//  MoogLadderFilter.m
+//  synth
+//
+//  Created by Nicholas Cardinal on 1/24/16.
+//  Copyright © 2016 nicholas.cardinal. All rights reserved.
+//
+
+#import "MoogLadderFilter.h"
+
+@implementation MoogLadderFilter
+
+@end
